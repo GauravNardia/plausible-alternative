ec115fceb154146c9e0d5f03e50f7c83ea45c885
Gaurav Nardia <codegaurav07@gmail.com>
2026-03-05 23:54:51 +0530

    feat: add normalizedomain and format number func

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,29 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export function normalizeDomain(input: string) {
+  return input
+    .replace(/^https?:\/\//, "") // remove http/https
+    .replace(/^www\./, "")       // remove www
+    .replace(/\/$/, "")          // remove trailing slash
+    .toLowerCase()
+}
+
+export function shortenEmail(email: string) {
+  const name = email.split("@")[0]
+  const short = name.slice(0, 6)
+  return `${short}@...`
+
+}
+
 export function formatNumber(num: number) {
-  if (num >= 1000) {
-    return (num / 1000).toFixed(1) + "k"
+  if (num >= 1_000_000) {
+    return (num / 1_000_000).toFixed(num % 1_000_000 === 0 ? 0 : 1) + "M"
   }
-  return num
+
+  if (num >= 1_000) {
+    return (num / 1_000).toFixed(num % 1_000 === 0 ? 0 : 1) + "K"
+  }
+
+  return num.toString()
 }
