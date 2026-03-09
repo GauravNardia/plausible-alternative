7002dae64d03e2f5f0ce9336a15b46ff208e0fa5
Gaurav Nardia <codegaurav07@gmail.com>
2026-03-09 22:40:28 +0530

    fix: type error in chart/statheader

--- components/charts/StatHeader.tsx
+++ components/charts/StatHeader.tsx
@@ -1,7 +1,7 @@
 export default function StatsHeader({ stats }: any) {
   return (
     <div className="grid grid-cols-4 gap-6 bg-white rounded-3xl border border-neutral-200 p-6 shadow-sm">
-      {Object.entries(stats).map(([key, value]) => (
+      {Object.entries(stats).map(([key, value]: any) => (
         <div key={key}>
           <p className="text-xs text-neutral-500 uppercase tracking-wide">
             {key}
